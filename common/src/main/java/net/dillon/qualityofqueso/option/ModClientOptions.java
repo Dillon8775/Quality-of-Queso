dd96176a855a31dc5894ad7e7d43bf264b005461
Dillon <dilloneystrickland12@gmail.com>
2026-05-30 14:24:28 -0400

    final changes!

--- common/src/main/java/net/dillon/qualityofqueso/option/ModClientOptions.java
+++ common/src/main/java/net/dillon/qualityofqueso/option/ModClientOptions.java
@@ -272,15 +272,15 @@ public class ModClientOptions {
         public boolean emptySlots = true;
         public boolean highlightArmor = true;
 
-        public int[] armorStatusPosition = new int[]{0, 0};
-        public int otherElementsY = 0;
-
         public boolean animations = true;
         public double animationTime = 1.5;
 
         public boolean coloredHighlighting = false;
         public boolean warningIndicators = false;
         public double displayTime = 4.0;
+
+        public int[] armorStatusPosition = new int[]{0, 0};
+        public int otherElementsY = 0;
     }
 
     public static class ItemCounterOptions {
@@ -288,8 +288,6 @@ public class ModClientOptions {
         public boolean countContainers = true;
         public boolean countEnderChest = false;
         public boolean onlyCountMatchingItems = false;
-
-        public int[] itemCounterPosition = new int[]{0, 0};
 
         public boolean arrowCounter = true;
         public boolean countAllArrows = true;
@@ -299,6 +297,9 @@ public class ModClientOptions {
         public boolean displayOnThrow = true;
         public boolean displayOnPickup = true;
         public boolean displayTotalWithStacks = false;
+
+        public int[] itemCounterPosition = new int[]{0, 0};
+        public boolean moveItemCounterOver = true;
     }
 
     public static class ElytraAlarmOptions {
