a4da16fe1676d4f17e9b850341e179fa5174f0f1
Dillon <dilloneystrickland12@gmail.com>
2026-05-30 12:57:03 -0400

    final changes!

--- common/src/main/java/net/dillon/qualityofqueso/screen/MainMenuScreen.java
+++ common/src/main/java/net/dillon/qualityofqueso/screen/MainMenuScreen.java
@@ -13,11 +13,10 @@ import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.debug.DebugOptionsScreen;
 import net.minecraft.network.chat.Component;
 
-import static net.dillon.qualityofqueso.helper.ModHelper.commonOptionsInstance;
-import static net.dillon.qualityofqueso.helper.ModHelper.modEnabled;
+import static net.dillon.qualityofqueso.helper.ModHelper.*;
 
 public class MainMenuScreen extends AbstractModScreen {
-    private AbstractWidget openItemFrameSearchGUIOptions, visualTime, debugHuds;
+    private AbstractWidget openItemFrameSearchGUIOptions, visualTime, hudPositions, debugHuds;
 
     public MainMenuScreen(Screen parent) {
         super(parent, Component.translatable("qualityofqueso.gui.options.title").withStyle(ChatFormatting.GOLD));
@@ -35,6 +34,12 @@ public class MainMenuScreen extends AbstractModScreen {
                 this.minecraft.setScreen(new VisualTimeScreen(this))
         ).tooltip(
                 Tooltip.create(Component.translatable("qualityofqueso.gui.visual_time.description"))
+        ).build();
+
+        this.hudPositions = Button.builder(Component.translatable("qualityofqueso.gui.hud_positions"), button -> {
+            this.minecraft.setScreen(new HudPositionsScreen(this));
+        }).tooltip(
+                Tooltip.create(Component.translatable("qualityofqueso.gui.hud_positions.tooltip"))
         ).build();
 
         this.debugHuds = Button.builder(Component.translatable("qualityofqueso.gui.debug_huds"), button -> {
@@ -70,13 +75,15 @@ public class MainMenuScreen extends AbstractModScreen {
 
                 this.visualTime,
 
+                this.debugHuds,
+
+                this.hudPositions,
+
                 Button.builder(Component.translatable("qualityofqueso.gui.resources"), button -> {
                     this.minecraft.setScreen(new ResourcesScreen(this));
                 }).tooltip(
                         Tooltip.create(Component.translatable("qualityofqueso.gui.resources.tooltip"))
                 ).build(),
-
-                this.debugHuds
         };
     }
 
@@ -85,6 +92,7 @@ public class MainMenuScreen extends AbstractModScreen {
         boolean modEnabled = modEnabled(this.minecraft);
         this.openItemFrameSearchGUIOptions.active = modEnabled && commonOptionsInstance().itemFrameSearching && this.minecraft.level != null;
         this.visualTime.active = modEnabled;
+        this.hudPositions.active = modEnabled && this.minecraft.level != null && (!clientOptionsInstance().getHudOptions().armorStatus.off() || clientOptionsInstance().getItemCounterOptions().itemCounter.enabled());
         this.debugHuds.active = modEnabled;
     }
 
--- common/src/main/java/net/dillon/qualityofqueso/screen/VisualTimeScreen.java
+++ common/src/main/java/net/dillon/qualityofqueso/screen/VisualTimeScreen.java
@@ -7,6 +7,7 @@ import net.dillon.qualityofqueso.util.VisualTimeTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.network.chat.Component;
@@ -23,7 +24,7 @@ import static net.dillon.qualityofqueso.helper.ModHelper.ofQoQ;
  */
 public class VisualTimeScreen extends Screen {
     private static final int SIZE = 24;
-    private AbstractWidget overrideClientTime, visualTime, visualTimeSpeed, matchWithIRLTime;
+    private AbstractWidget overrideClientTime, visualTime, visualTimeSpeed, syncLocalTime;
     private final Screen parent;
 
     public VisualTimeScreen(Screen parent) {
@@ -42,20 +43,24 @@ public class VisualTimeScreen extends Screen {
     }
 
     @Override
-    public void extractBackground(GuiGraphicsExtractor graphics, int mouseX, int mouseY, float deltaTicks) {
-        if (this.minecraft.level == null) {
-            this.extractPanorama(graphics, deltaTicks);
-        }
-        this.extractMenuBackground(graphics);
-    }
-
-    @Override
     protected void init() {
         int width = this.width / 2 - 32;
         this.overrideClientTime = this.addRenderableWidget(ListOptions.overrideClientTime().createButton(Minecraft.getInstance().options, this.width / 2 - 100, this.height / 2 - 48, 200));
         this.visualTime = this.addRenderableWidget(ListOptions.visualTime().createButton(Minecraft.getInstance().options, width, this.overrideClientTime.getY() + 32, 120));
         this.visualTimeSpeed = this.addRenderableWidget(ListOptions.visualTimeSpeed().createButton(Minecraft.getInstance().options, width, this.visualTime.getY() + 28, 120));
-        this.matchWithIRLTime = this.addRenderableWidget(ListOptions.syncLocalTime().createButton(Minecraft.getInstance().options, width, this.visualTimeSpeed.getY() + 28, 120));
+        this.syncLocalTime = this.addRenderableWidget(ListOptions.syncLocalTime().createButton(Minecraft.getInstance().options, width, this.visualTimeSpeed.getY() + 28, 120));
+
+        this.addRenderableWidget(Button.builder(Component.translatable("qualityofqueso.gui.save_changes"), button -> {
+            this.onClose();
+        }).bounds(this.syncLocalTime.getX() + 12, this.syncLocalTime.getY() + 32, 100, 20).build());
+    }
+
+    @Override
+    public void extractBackground(GuiGraphicsExtractor graphics, int mouseX, int mouseY, float deltaTicks) {
+        if (this.minecraft.level == null) {
+            this.extractPanorama(graphics, deltaTicks);
+        }
+        this.extractMenuBackground(graphics);
     }
 
     @Override
@@ -72,13 +77,13 @@ public class VisualTimeScreen extends Screen {
         graphics.blit(RenderPipelines.GUI_TEXTURED, clockTexture, this.visualTime.getX() - 48, this.visualTime.getY() - 2, 0.0F, 0.0F, SIZE, SIZE, SIZE, SIZE);
 
         graphics.blitSprite(RenderPipelines.GUI_TEXTURED, ofQoQ("visual_time/speed"), this.visualTimeSpeed.getX() - 48, this.visualTimeSpeed.getY() + 1, 24, 18);
-        graphics.blit(RenderPipelines.GUI_TEXTURED, Identifier.withDefaultNamespace("textures/block/daylight_detector" + (clientOptionsInstance().getVisualTimeOptions().syncLocalTime ? "_inverted" : "") + "_top.png"), this.matchWithIRLTime.getX() - 48, this.matchWithIRLTime.getY() - 2, 0.0F, 0.0F, SIZE, SIZE, SIZE, SIZE);
+        graphics.blit(RenderPipelines.GUI_TEXTURED, Identifier.withDefaultNamespace("textures/block/daylight_detector" + (clientOptionsInstance().getVisualTimeOptions().syncLocalTime ? "_inverted" : "") + "_top.png"), this.syncLocalTime.getX() - 48, this.syncLocalTime.getY() - 2, 0.0F, 0.0F, SIZE, SIZE, SIZE, SIZE);
 
         boolean overrideClientTime = ModHelper.clientOptionsInstance().getVisualTimeOptions().overrideClientTime;
         boolean matchWithIRLTime = ModHelper.clientOptionsInstance().getVisualTimeOptions().syncLocalTime;
         this.visualTime.active = overrideClientTime && ModHelper.clientOptionsInstance().getVisualTimeOptions().visualTimeSpeed == 0 && !matchWithIRLTime;
         this.visualTimeSpeed.active = overrideClientTime && !matchWithIRLTime;
-        this.matchWithIRLTime.active = overrideClientTime;
+        this.syncLocalTime.active = overrideClientTime;
 
         this.extractBlurredBackground(graphics);
     }
