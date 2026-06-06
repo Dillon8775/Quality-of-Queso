7274eb4505eaa3686767a8f42986b0ebc68fc436
Dillon <dilloneystrickland12@gmail.com>
2026-06-06 11:06:09 -0400

    final update

--- common/src/main/java/net/dillon/qualityofqueso/screen/ResourcesScreen.java
+++ common/src/main/java/net/dillon/qualityofqueso/screen/ResourcesScreen.java
@@ -39,11 +39,6 @@ public class ResourcesScreen extends AbstractModScreen {
                         Tooltip.create(Component.translatable("qualityofqueso.gui.resource_pack_template.tooltip"))
                 ).build(),
 
-                Button.builder(Component.translatable("qualityofqueso.gui.video_explanations"),
-                        ConfirmLinkScreen.confirmLink(this, ModConstants.VIDEO_EXPLANATIONS_LINK, false)
-                ).tooltip(
-                        Tooltip.create(Component.translatable("qualityofqueso.gui.video_explanations.tooltip"))
-                ).build(),
                 Button.builder(Component.translatable("qualityofqueso.gui.other_qol_mods"),
                         ConfirmLinkScreen.confirmLink(this, ModConstants.OTHER_QOL_MODS_LINK, false)
                 ).tooltip(
