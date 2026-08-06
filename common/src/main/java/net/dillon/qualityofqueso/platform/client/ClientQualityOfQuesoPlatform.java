443bb1900ff1d7a7eb835d32266ab87c1091a04c
Dillon <dilloneystrickland12@gmail.com>
2026-08-05 23:21:09 -0400

    finalize

--- common/src/main/java/net/dillon/qualityofqueso/platform/client/ClientQualityOfQuesoPlatform.java
+++ common/src/main/java/net/dillon/qualityofqueso/platform/client/ClientQualityOfQuesoPlatform.java
@@ -1,12 +1,20 @@
 package net.dillon.qualityofqueso.platform.client;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import net.dillon.dillonlib.platform.client.ClientModPlatform;
 import net.dillon.qualityofqueso.util.ModConstants;
+import net.minecraft.client.KeyMapping;
 
 public abstract class ClientQualityOfQuesoPlatform extends ClientModPlatform {
 
     @Override
     public String modId() {
         return ModConstants.MOD_ID;
+    }
+
+    // Unused
+    @Override
+    public KeyMapping registerKeyMapping(String name, InputConstants.Type type, KeyMapping.Category category, int value) {
+        return null;
     }
 }
